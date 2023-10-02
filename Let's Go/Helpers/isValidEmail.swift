0134b8be8b8843be3f42b0790a5d9c8e361825b0
IvanBogdantsev <bogdantsev.st@gmail.com>
2023-10-02 11:40:31 +0200

    fixed email regex to allow one character after dot

--- Let's Go/Helpers/isValidEmail.swift
+++ Let's Go/Helpers/isValidEmail.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-private let emailRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{2,7})*$"
+private let emailRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{1,7})*$"
 
 func isValidEmail(_ email: String) -> Bool {
     let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
-    return predicate.evaluate(with: email)  // 3
+    return predicate.evaluate(with: email) 
 }
